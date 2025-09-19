4a85310e5872830efcdfbcf4513c212134e2914a
enzobalderrama <76264578+enzobalderrama@users.noreply.github.com>
2025-09-18 23:22:14 -0600

    Fixed Lab 6

--- src/packageName/ParkTest.java
+++ src/packageName/ParkTest.java
@@ -1,13 +1,33 @@
 package packageName;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class ParkTest {
 
-	public static void main(String[] args) {
-		Campsite thisCampsite = new Campsite(10.0);
-		Trail thisTrail = new Trail(25.0, 6.0);
-		//Obviously there are a lot of options of how to do this but I went off GitHub's example
-		System.out.println("Park Area: " + (thisCampsite.area() + thisTrail.area()));
-
+	public static void main(String[] args) throws IOException {
+		File file = new File("file.txt");
+        Scanner sc = new Scanner(file);
+        String nex = "";
+        double radius = 0.0;
+        double length = 0.0;
+        double width = 0.0;
+        double parkArea = 0;
+        while(sc.hasNextLine()){
+            nex = sc.next();
+            if (nex.equals("campsite")) {
+            	radius = sc.nextDouble();
+            	parkArea += Math.PI* Math.pow(radius, 2);
+            }
+            else if(nex.equals("trail")) {
+            	length = sc.nextDouble();
+            	width = sc.nextDouble();
+            	parkArea += length * width;
+            }
+        }
+        sc.close();
+        System.out.println(parkArea);
 	}
 
 }
